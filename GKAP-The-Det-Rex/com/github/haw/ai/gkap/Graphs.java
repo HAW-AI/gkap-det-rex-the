4b863d766159184777bd6f17a3d96e45fcdc3a06
Ben <salzig+github@gmail.com>
2011-10-30 23:05:38 +0100

    Basic Interfaces

---
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/Graphs.java
@@ -0,0 +1,10 @@
+package com.github.haw.ai.gkap;
+
+/**
+ * @author Till Theis <till.theis@haw-hamburg.de>
+ * @author Patrick Detlefsen <patrick.detlefsen@haw-hamburg.de>
+ * @author Benjamin Rexin <benjamin.rexin@haw-hamburg.de>
+ */
+public class Graphs {
+
+}
---
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/Edge.java
@@ -0,0 +1,10 @@
+package com.github.haw.ai.gkap.graph;
+
+/**
+ * @author Till Theis <till.theis@haw-hamburg.de>
+ * @author Patrick Detlefsen <patrick.detlefsen@haw-hamburg.de>
+ * @author Benjamin Rexin <benjamin.rexin@haw-hamburg.de>
+ */
+public interface Edge<E,V> {
+
+}
