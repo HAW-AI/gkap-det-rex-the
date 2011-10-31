5d0279b9de8ac0de59b34597618980a5ab4e1c08
Ben <salzig+github@gmail.com>
2011-10-31 22:11:42 +0100

    fix text indention and type casting

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/VertexImpl.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/VertexImpl.java
@@ -6,44 +6,46 @@ package com.github.haw.ai.gkap.graph;
  * @author Benjamin Rexin <benjamin.rexin@haw-hamburg.de>
  */
 public class VertexImpl<V> implements Vertex<V> {
-  private V content;	
+	private V content;
 
-  private VertexImpl(V content) {
-    this.content = content;
-  }
+	private VertexImpl(V content) {
+		this.content = content;
+	}
 
-  public static Vertex<V> valueOf(V content) {
+	public static Vertex<V> valueOf(V content) {
 		return new VertexImpl<V>(content);
 	}
-  public V content() {
-    return content;
-  }
 
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((content == null) ? 0 : content.hashCode());
-    return result;
-  }
+	public V content() {
+		return content;
+	}
 
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null) {
-      return false;
-    }
-    if (!(o instanceof Vertex)) {
-      return false;
-    }
-    if (content == null) {
-      if ((Vertex o).content() != null) {
-        return false;
-      }
-    }
-    if (!content.equals((Vertex o).content) {
-      return false;
-    }
-    return true;
-  }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null) {
+			return false;
+		}
+		if (!(o instanceof Vertex)) {
+			return false;
+		}
+		if (content == null) {
+			if (((Vertex<V>) o).content() != null) {
+				return false;
+			}
+		}
+		if (!content.equals(((Vertex<V>) o).content())) {
+			return false;
+		}
+		return true;
+	}
 }
