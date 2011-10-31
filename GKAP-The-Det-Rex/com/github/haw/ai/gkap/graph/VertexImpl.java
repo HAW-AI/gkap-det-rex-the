300b06f4833935c3fede85fdc9e09d9e24d160b3
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-10-31 19:11:20 +0100

    VertexImpl initial version

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/VertexImpl.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/VertexImpl.java
@@ -6,10 +6,44 @@ package com.github.haw.ai.gkap.graph;
  * @author Benjamin Rexin <benjamin.rexin@haw-hamburg.de>
  */
 public class VertexImpl<V> implements Vertex<V> {
-	public Vertex<V> valueOf(V content) {
+  private V content;	
+
+  private VertexImpl(V content) {
+    this.content = content;
+  }
+
+  public Vertex<V> valueOf(V content) {
 		return new VertexImpl<V>(content);
 	}
-	private VertexImpl(V content) {
-		
-	}
+  public V content() {
+    return content;
+  }
+
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((content == null) ? 0 : content.hashCode());
+    return result;
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    if (!(o instanceof Vertex)) {
+      return false;
+    }
+    if (content == null) {
+      if ((Vertex o).content() != null) {
+        return false;
+      }
+    }
+    if (!content.equals((Vertex o).content) {
+      return false;
+    }
+    return true;
+  }
 }
