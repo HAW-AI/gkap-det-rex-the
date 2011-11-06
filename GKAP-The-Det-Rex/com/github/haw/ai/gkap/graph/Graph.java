63a4e8ab0f3d03fc2a750c5673f180310dd78dde
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-11-06 18:40:03 +0100

    improved documentation

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/Graph.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/Graph.java
@@ -10,26 +10,31 @@ import java.util.Set;
 public interface Graph<E,V> {
 
   /**
+   * @param A Vertex of which the degree should be calculated
    * @return the degree of a given Vertex
    */
   public int degree(Vertex<V> vertex);
 
   /**
+   * @param two vertices that may or may not be adjacent
    * @return true if the two given vertices are adjacent
    */
   public boolean isAdjacent(Vertex<V> vertex, Vertex<V> otherVertex);
 
   /**
+   * @param A Vertex of which the Set of adjacent Vertices is calculated
    * @return the Set of Vertices that are adjacent to the given Vertex
    */
   public Set<Vertex<V>> adjacent(Vertex<V> vertex);
 
   /**
+   * @param A Vertex and Edge which are checked for their incidence
    * @return true if the vertex is incident to the given edge
    */
   public boolean isIncident(Vertex<V> vertex, Edge<E,V> edge);
 
   /**
+   * @param A Vertex whose Set of incident Edges will be calculated
    * @return the Set of Edges that are incident with the given Vertex
    */
   public Set<Edge<E,V>> incident(Vertex<V> vertex);
