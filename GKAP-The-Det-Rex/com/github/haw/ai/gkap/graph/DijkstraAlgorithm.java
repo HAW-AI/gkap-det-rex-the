398904696214f94d97685b8cb547148ac0b8e99f
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-11-21 16:56:02 +0100

    Dijkstra Algorithm

---
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/DijkstraAlgorithm.java
@@ -0,0 +1,126 @@
+package com.github.haw.ai.gkap.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DijkstraAlgorithm<E,V> {
+
+	private Graph<E,V> graph;
+	private Map<Vertex<V>, Double> distances;
+	private Map<Vertex<V>,Vertex<V>> predecessor;
+	private Map<Vertex<V>, Boolean> ok;
+	
+	private DijkstraAlgorithm(Graph<E,V> graph) {
+		this.graph = graph;
+	}
+
+	public DijkstraAlgorithm<E,V> valueOf(Graph<E,V> graph) {
+		return new DijkstraAlgorithm<E,V>(graph);
+	}
+	
+	public void runAlgorithm(Vertex<V> start, Vertex<V> finish) {
+		if (start == null || finish == null || !graph.vertices().contains(start) || !graph.vertices().contains(finish)) {
+			throw new IllegalArgumentException("Something went wrong with your start nodes.");
+		}
+		/*
+		 * Edge contents must be numbers
+		 */
+		for (Edge<E, V> edge : graph.edges()) {
+			if (!(edge.content() instanceof Integer) || !(edge.content() instanceof Double)) {
+				throw new IllegalArgumentException("All of your Edge contents have to be Numbers because they are the weight used to calculate the shortest path.");
+			}
+		}
+
+		/*
+		 * The setup stage
+		 */
+		setup(start);
+		
+		/*
+		 * The iteration Stage
+		 */
+		Vertex<V> currentVertex = null;
+		Set<Edge<E, V>> incidentEdges = null;
+		while (ok.entrySet().contains(false)) {
+			
+			/*
+			 * Get the unvisited Vertex with the lowest distance..
+			 */
+			currentVertex = getUnvisitedVertexWithLowestDistance();
+			
+			/*
+			 * ..and set this Vertex as Visited.
+			 */
+			ok.put(currentVertex, true);
+			
+			// get the possible Edges we could possibly walk along from the current Vertex
+			incidentEdges = graph.incident(currentVertex);
+			for (Edge<E, V> e : incidentEdges) {
+				// if the Distance is lower than the currently know distance:
+				if ((Double) e.content() + distances.get(currentVertex) < distances.get(e.otherVertex(currentVertex))) {
+					// set the new, shorter distance
+					distances.put(e.otherVertex(currentVertex), ((Double) e.content() + distances.get(currentVertex)));
+					// and set yourself as the predecessor
+					predecessor.put(e.otherVertex(currentVertex), currentVertex);
+				}
+			}
+		}		
+	}
+	
+	private Vertex<V> getUnvisitedVertexWithLowestDistance() {
+		Vertex<V> result = null;
+		
+		Set<Vertex<V>> unvisited = new HashSet<Vertex<V>>();
+		for (Vertex<V> vert : ok.keySet()) {
+			if (ok.get(vert)) {
+				unvisited.add(vert);
+			}
+		}
+		
+		for (Vertex<V> vert : unvisited) {
+			if (result == null) {
+				result = vert;
+			}
+			if (distances.get(vert) < distances.get(result)) {
+				result = vert;
+			}
+		}
+		
+		return result;
+	}
+
+	/*
+	 * This Method sets the distances map, the predecessors map and the ok map up
+	 * with the required initial values so that the findShortestPath method can 
+	 * start the second stage of the Dijkstra Algorithm.
+	 */
+	private void setup(Vertex<V> startVertex) {
+		distances = new HashMap<Vertex<V>, Double>();
+		predecessor = new HashMap<Vertex<V>, Vertex<V>>();
+		ok = new HashMap<Vertex<V>, Boolean>();
+		
+		for (Vertex<V> vertex : graph.vertices()) {
+			distances.put(vertex, Double.POSITIVE_INFINITY);
+			ok.put(vertex, false);
+		}
+		distances.put(startVertex, 0.0);
+		predecessor.put(startVertex, startVertex);
+		
+		/*
+		 * All the undirected Edges have to be replaced with two directed
+		 * opposing Edges
+		 */
+		Set<Edge<E, V>> updatedEdges = new HashSet<Edge<E, V>>();
+		for (Edge<E,V> edge : graph.edges()) {
+			if (edge.isDirected()) {
+				updatedEdges.add(edge);
+			} else {
+				Graphs.directedEdge(edge.left(), edge.right(), edge.content());
+				Graphs.directedEdge(edge.right(), edge.left(), edge.content());
+			}
+		}
+		graph = GraphImpl.valueOf(updatedEdges, graph.vertices());
+	}
+}
