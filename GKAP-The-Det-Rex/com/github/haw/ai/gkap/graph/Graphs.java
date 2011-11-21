32e00170fdd596a4431b89d3ce8f0920b02f6a20
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2011-11-21 18:55:56 +0100

    added a factory method for graphs

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/Graphs.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/graph/Graphs.java
@@ -1,5 +1,7 @@
 package com.github.haw.ai.gkap.graph;
 
+import java.util.Set;
+
 public class Graphs {
     public static <E, V> Edge<E, V> directedEdge(Vertex<V> left, Vertex<V> right, E content) {
         return DirectedEdge.valueOf(left, right, content);
@@ -11,5 +13,9 @@ public class Graphs {
     
     public static <V> Vertex<V> vertex(V content) {
     	return VertexImpl.valueOf(content);
+    }
+    
+    public static <E, V> Graph<E, V> graph(Set<Edge<E, V>> edges, Set<Vertex<V>> vertices) {
+    	return GraphImpl.valueOf(edges, vertices);
     }
 }
