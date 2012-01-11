cd73ba6eea17fdd4dc404fc07eb508e149cb68ce
Till Theis <till_theis2@yahoo.de>
2012-01-11 11:58:40 +0100

    first hamiltonian path impl

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/algorithms/HamiltonianPath.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/algorithms/HamiltonianPath.java
@@ -7,6 +7,37 @@ import com.github.haw.ai.gkap.graph.*;
 
 public class HamiltonianPath {
     public static <V> List<Vertex<V>> hamiltonianPath(Graph<?, V> graph) {
+        List<Vertex<V>> vs = new ArrayList<Vertex<V>>(graph.vertices());
+        if (vs.size() <= 1) return vs;
+        List<Integer> idxs = new ArrayList<Integer>(vs.size());
+        for (int i = 0; i < vs.size(); ++i) idxs.add(i);
+        PermutationIterator<Integer> iter = new PermutationIterator<Integer>(idxs);
+        
+        while (iter.hasNext()) {
+            List<Integer> path = iter.next();
+            
+            boolean checkNextPerm = false;
+            
+            for (int i = 0; i < path.size()-1 && !checkNextPerm; ++i) {
+                int j = path.get(i);
+                int k = path.get(i+1);
+                if (!graph.isAdjacent(vs.get(j), vs.get(k))) {
+                    checkNextPerm = true;
+                }
+            }
+            
+            if (!checkNextPerm) {
+                // found valid path
+                return idxsToVs(path, vs);
+            }
+        }
+        
         return new ArrayList<Vertex<V>>();
+    }
+    
+    private static <V> List<Vertex<V>> idxsToVs(List<Integer> idxs, List<Vertex<V>> vs) {
+        List<Vertex<V>> path = new ArrayList<Vertex<V>>(idxs.size());
+        for (int idx : idxs) path.add(vs.get(idx));
+        return path;
     }
 }
