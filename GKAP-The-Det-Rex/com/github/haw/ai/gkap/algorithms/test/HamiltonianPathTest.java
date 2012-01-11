e197de7d0c90c251509379f5ffafc41dc0bfca70
Till Theis <till_theis2@yahoo.de>
2012-01-11 12:09:40 +0100

    add negative test

--- GKAP-The-Det-Rex/com/github/haw/ai/gkap/algorithms/test/HamiltonianPathTest.java
+++ GKAP-The-Det-Rex/com/github/haw/ai/gkap/algorithms/test/HamiltonianPathTest.java
@@ -17,8 +17,9 @@ import static com.github.haw.ai.gkap.graph.Graphs.*;
 import static com.github.haw.ai.gkap.algorithms.HamiltonianPath.hamiltonianPath;
 
 public class HamiltonianPathTest {
-    
+
     Graph<String, String> graph;
+    Graph<String, String> graph1;
     
 
     @Before
@@ -29,6 +30,9 @@ public class HamiltonianPathTest {
         es.add(undirectedEdge(vertex("V2"), vertex("V3"), "E2"));
         es.add(undirectedEdge(vertex("V3"), vertex("V4"), "E3"));
         es.add(undirectedEdge(vertex("V4"), vertex("V1"), "E4"));
+        // explicit copy because Graph#Graph doesn't create copies...
+        graph1 = graph(new HashSet<Edge<String,String>>(es), new HashSet<Vertex<String>>(vs));
+        
         es.add(undirectedEdge(vertex("V1"), vertex("V5"), "E5"));
         es.add(undirectedEdge(vertex("V2"), vertex("V5"), "E6"));
         es.add(undirectedEdge(vertex("V3"), vertex("V5"), "E7"));
@@ -37,9 +41,12 @@ public class HamiltonianPathTest {
     }
 
     @Test
-    public void test() {
+    public void testHamiltonianPath() {
         List<Vertex<String>> path = hamiltonianPath(graph);
         assertTrue(path.containsAll(graph.vertices()) && path.size() == graph.vertices().size());
+        
+        List<Vertex<String>> path1 = hamiltonianPath(graph1);
+        assertTrue(path1.isEmpty());
     }
 
 }
